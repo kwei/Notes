dd871e8ddb8de250fd1653c133240269bce8f061
kw_yeh <kw_yeh@cyberlink.com>
2024-03-11 18:26:54 +0800

    Add Prism highlight for code block

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./prism.css";
-import "./prism";
 import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
