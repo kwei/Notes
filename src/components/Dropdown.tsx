12aa25804634e48b811acdfe693de493330a8cd1
kw_yeh <kw_yeh@cyberlink.com>
2024-03-20 09:51:37 +0800

    Adjust UI

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -26,19 +26,6 @@ export const Dropdown = (props: Props) => {
     setOpen(false);
   });
 
-  function calculateDropdownHeight(element: HTMLElement | null) {
-    const marginValue = 8;
-    const paddingValue = 20;
-    if (typeof window === "undefined") return 200;
-    if (!element) return window.innerHeight * (2 / 5);
-    return (
-      window.innerHeight -
-      element.getBoundingClientRect().bottom -
-      marginValue -
-      paddingValue
-    );
-  }
-
   const handleTriggerMenu = useCallback(() => {
     setOpen((prevState) => !prevState);
   }, []);
@@ -104,6 +91,19 @@ const Option = (props: OptionProps) => {
 };
 
 Dropdown.Option = Option;
+
+function calculateDropdownHeight(element: HTMLElement | null) {
+  const marginValue = 8;
+  const paddingValue = 20;
+  if (typeof window === "undefined") return 200;
+  if (!element) return window.innerHeight * (2 / 5);
+  return (
+    window.innerHeight -
+    element.getBoundingClientRect().bottom -
+    marginValue -
+    paddingValue
+  );
+}
 
 const DropdownContext = createContext<{
   value?: string;
