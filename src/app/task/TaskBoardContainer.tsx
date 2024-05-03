38a3081ae5844ac2ccdcc344c2ab07bfdb0465db
kw_yeh <kw_yeh@cyberlink.com>
2024-05-03 14:35:21 +0800

    Adjust UI
    [TODO] Save Meta Info

--- src/app/task/TaskBoardContainer.tsx
+++ src/app/task/TaskBoardContainer.tsx
@@ -7,7 +7,7 @@ export const TaskBoardContainer = async () => {
   return (
     <DraggableTask>
       <ToolCtxProvider>
-        <div className="flex w-full flex-1 flex-col items-center max-w-[1480px] bg-gray-800 xl:rounded-3xl rounded-b-none p-5 gap-3">
+        <div className="flex w-full flex-1 flex-col items-center max-w-[1480px] bg-gray-800 xl:rounded-3xl rounded-b-none max-xl:px-2 p-5 gap-3">
           <ToolBox />
           <TaskBoard />
         </div>
