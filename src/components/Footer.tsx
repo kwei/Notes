5c81c71903895989adc4fa628620bb8a6531c8d1
kw_yeh <kw_yeh@cyberlink.com>
2024-05-06 17:49:11 +0800

    Footer wording
    [TODO] Save Meta Info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export const Footer = () => {
 	return (
-		<div className='w-full p-5 flex flex-col items-center justify-center'>
+		<div className='w-full p-5 flex flex-col items-start md:items-center justify-center'>
 			<span>Copyright © 2024 KW Yeh. All rights reserved.</span>
 			<span>版權所有© 2024 KW Yeh</span>
 		</div>
