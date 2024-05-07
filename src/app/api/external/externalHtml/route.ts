d0ec6e7c07c6ab18b09fcda075dfc7f21051bc0b
kw_yeh <kw_yeh@cyberlink.com>
2024-05-07 17:04:10 +0800

    preview link
    [TODO] Save Meta Info

--- src/app/api/external/externalHtml/route.ts
+++ src/app/api/external/externalHtml/route.ts
@@ -5,9 +5,15 @@ export async function GET(req: Request) {
   console.log("req url: ", req.url);
   const queryList = req.url.split("?")[1]?.split("&") ?? [];
   const link = getQueryString(queryList, "link");
-  if (link) {
-    const res = await fetch(link);
-    return NextResponse.json(await res.text());
+  try {
+    if (link) {
+      console.log("external url: ", link);
+      const res = await fetch(link);
+      return NextResponse.json(await res.text());
+    }
+  } catch (e) {
+    console.log("fetch external url failed: ", e);
+    return NextResponse.json("");
   }
   return NextResponse.json("");
 }
