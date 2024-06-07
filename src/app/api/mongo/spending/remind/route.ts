3545c900fdf021ac5a842cf74df66e04ae5e4d6a
kw_yeh <kw_yeh@cyberlink.com>
2024-06-07 15:41:32 +0800

    Test Web Push
    not yet: view yearly list

--- src/app/api/mongo/spending/remind/route.ts
+++ src/app/api/mongo/spending/remind/route.ts
@@ -6,12 +6,6 @@ const vapidKeys = {
   privateKey: process.env.VAPIDKEY_PRIVATE ?? "",
 };
 
-webPush.setVapidDetails(
-  "mailto:a0979597291@gmail.com",
-  vapidKeys.publicKey,
-  vapidKeys.privateKey,
-);
-
 const options = {
   title: "Test Web Push",
   body: "Test Web Push...",
@@ -20,23 +14,29 @@ const options = {
 export async function POST(req: Request) {
   console.log("[POST] req url: ", req.url);
 
-  // read all subscriptions
-  const subscription = {
-    endpoint:
-      "https://fcm.googleapis.com/fcm/send/fwNbCkZtyr0:APA91bF-tttRSH0KBHuZ3lGehkd7kcNzWOfAVTKeXp4cYUURgq2bEkTkCtLQAvrzDZ7q_N7on0ved-Ss9SGLRYGm61D2rkmPe2R2EUnLn7s1y7Fwrjts2I-qM94SQINyJA4VBV5spTdy",
-    expirationTime: null,
-    keys: {
-      p256dh:
-        "BEClFpLV0UV65m9wtmPxLVN6kIUTrALuRlUANPySm3eSXXArabumm4aziX4tPYefRtDaGpqL8SxwyyJlc9BvJzA=",
-      auth: "I4qBuGIBj1pUVbT4sQnHEg==",
-    },
-  };
+  if (vapidKeys.publicKey && vapidKeys.privateKey) {
+    webPush.setVapidDetails(
+      "mailto:a0979597291@gmail.com",
+      vapidKeys.publicKey,
+      vapidKeys.privateKey,
+    );
 
-  webPush
-    .sendNotification(subscription, JSON.stringify(options))
-    .then((res) => {
-      console.log("Send Notification: ", res);
-    });
+    // read all subscriptions
+    const subscription = {
+      endpoint: "",
+      expirationTime: null,
+      keys: {
+        p256dh: "",
+        auth: "",
+      },
+    };
+
+    webPush
+      .sendNotification(subscription, JSON.stringify(options))
+      .then((res) => {
+        console.log("Send Notification: ", res);
+      });
+  }
 
   return NextResponse.json({
     status: true,
