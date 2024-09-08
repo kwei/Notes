8edf13f16dd9fe9da5482b50bf3f223d4cf7cd35
KW <a0979597291@gmail.com>
2024-09-08 18:47:22 +0800

    Update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>KW Page</title>
+        <title>Life Helper</title>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#ffffff" />
@@ -42,10 +42,26 @@ export default async function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
-      <body
-        className={`relative h-dvh w-full ${inter.className}`}
-      >
+      <body className={`relative h-dvh w-full ${inter.className}`}>
         <Suspense>
           <div className="flex h-full w-full flex-col">
             <Header />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect("/spending");
+  return <div className="w-full flex-1"></div>;
 }
